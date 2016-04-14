05a4a34b98d0c6d672bb2acff20041bedd7b036b
ekomunthe48 <gudepsman.1silaen@gmail.com>
2016-04-14 14:34:44 +0700

    Bebas

---
+++ contoh-commit-master/Fighting/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fighting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Hero hero1;
+        static void setNewStatus(Label label4, Label label3, ProgressBar progressBar4, ProgressBar progressBar3,Hero hero1)
+        {
+            label4.Text = "HP : " + hero1.getHp() + " / " + hero1.getMaxHp();
+            label3.Text = "MP : " + hero1.getMp() + " / " + hero1.getMaxMp();
+            progressBar4.Value = (int)((float)hero1.getHp() / hero1.getMaxHp() * 100);
+            progressBar3.Value = (int)((float)hero1.getMp() / hero1.getMaxMp() * 100);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            hero1 = new Hero(1);
+            setNewStatus(label4, label3, progressBar4, progressBar3, hero1);
+            //this.Text = hero1.getHp().ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Command c = new Command();
+            c.ShowDialog();
+            textBox2.AppendText(c.getCommand());
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Heal_Spell_List h = new Heal_Spell_List(hero1.getLevel());
+            h.ShowDialog();
+            textBox2.AppendText(h.getHealLevel().ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            List<string> s = hero1.getList();
+            Use_Item u = new Use_Item(ref s);
+            u.ShowDialog();
+            hero1.setList(s);
+        }
+    }
+}
---
+++ contoh-commit-master/Fighting/Use Item.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Fighting
+{
+    partial class Use_Item
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Source Code Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(112, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 43);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Source Code Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Source Code Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 9);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 184);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Use_Item
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Use_Item";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Use_Item";
+            this.Load += new System.EventHandler(this.Use_Item_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
